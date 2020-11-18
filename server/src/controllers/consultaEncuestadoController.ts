d2b8fab7e3801434c0aa80a7cc47b022988b5478
maag070208 <asael070208@gmail.com>
2020-11-17 17:58:37 -0800

    consulting route add

--- server/src/controllers/consultaEncuestadoController.ts
+++ server/src/controllers/consultaEncuestadoController.ts
@@ -6,8 +6,8 @@ class ConsultaEncuestadoController {
   public async insert(req: Request, res: Response): Promise<void> { 
       const {nombre} = req.body;   
       const sql = `SELECT  NOMBRE, APELLIDOP, APELLIDOM, TELEFONO_CASA, CELULAR, TIPO_CELULAR, CORREO, CURP, DOMICILIO, VIGENCIA, DISTRITO_ESTATAL, DISTRITO_FEDERAL, SECCION, ID FROM DATOS_ENCUESTADO WHERE (NOMBRE LIKE '%${nombre}%')`;
-    await  pool.query(sql);
-    res.json({'message': 'Encontrado'});
+    const result =  await  pool.query(sql);
+     res.json(result);
    }
 
 
